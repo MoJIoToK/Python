d1403fd4d5eb18a31718e9f25d38b587ed7cf5cd
Nick <mojiotok167@gmail.com>
2022-12-18 16:21:03 +0300

    добавил модуль меню к информационной системе

--- 8S_HW/menu.py
+++ 8S_HW/menu.py
@@ -1,68 +1,84 @@
-import entry
-# import read
-# import control
+import teacher
+import student
+import autorization as au
 import logger as lg
+import admin
 
 print('Добро пожаловать! Вас приветствует информационная система "Школик"')
 
 
 def Menu():
-
     flag = True
 
     while flag:
-        print('Выберите и введите необходимое дейтсвие:\n'
-                '1 - Преподаватель\n'
-                '2 - Ученик\n'
-                '3 - Администратор\n'
-                '4 - Написать администратору\n'
-                '5 - Выход\n')
-        command = int(input('Выберите команду: '))
-
-        if command == 1:
-            # message = ''
-            lg.Log('User has selected command 1')
-            #password = input('Введите пароль: ')
-            lg.Log(f'User has entered password;')
-            cnt = entry.Password(command, "Petrov")
-            if cnt == True:
-                print(True) 
-                subcommand1 = input(('Выберите предмет:\n'
-                        '1 - Русский язык\n'
-                        '2 - Математика\n'))
-                subcommand2 = input(('Выберите класс:\n'
-                        '1 - 5А\n'
-                        '2 - 10Б\n'))
-                subcommand3 = input(('Выберите действие:\n'
-                        '1 - Поставить оценки\n'
-                        '2 - Написать ДЗ\n'))
-                entry.Enter_info(subcommand1, subcommand2, subcommand3)
-            else:
-                password = input('Пароль неверный, попробуйте снова: ')
-                entry.Password(command, password)
+        print('Введите логин и пароль либо пробелы для выхода:\n'
+                '1 - Авторизоваться\n'
+                '2 - Выход\n')
+        login = str(input('Введите логин: '))
         
-        if command == 2:
-            lg.Log('User has selected command 1')
-            password = input('Введите пароль: ')
-            lg.Log(f'User has entered password;')
-            entry.Password(command, password)
-            if cnt == True: 
-                subcommand1 = input(('Выберите предмет:\n'
-                        '1 - Русский язык\n'
-                        '2 - Математика\n'))
-                subcommand2 = input(('Выберите действие:\n'
-                        '1 - Посмотреть оценки\n'
-                        '2 - Посмотреть ДЗ\n'))
-                entry.Enter_info(subcommand1, subcommand2)
-            else:
-                while cnt != True:
-                    password = input('Пароль неверный, попробуйте снова: ')
-                    entry.Password(command, password)
+        lg.Log(f'User has entered login - {login}')
+        password = str(input('Введите пароль: '))
+        lg.Log(f'User has entered password - {password}')
+        
+        status = au.control(login, password)
+        print(status)
 
-        if command == 5:
-            lg.Log('User has selected command 5')
+        if status == ' Teacher':
+            print('Вы вошли как преподаватель. Введите необходимое действие:\n'
+            '1 - Написание ДЗ\n'
+            '2 - Выставление оценок\n'
+            '3 - Запрос админу\n'
+            '4 - Выход')
+            command = int(input('Введите действие: '))
+
+            while command != 4:
+                if command == 1:
+                    message = str(input('Введите ДЗ: '))
+                    teacher.write_HW(message)
+                if command == 2:
+                    teacher.write_score()
+                if command == 3:
+                    message = str(input('Введите запрос: '))
+                    teacher.request_admin(message)
+        
+        if status == ' Student':
+            print('Вы вошли как ученик. Введите необходимое действие:\n'
+            '1 - Просмотр оценок\n'
+            '2 - Просмотр ДЗ\n'
+            '3 - Запрос админу\n'
+            '4 - Выход')
+            command = int(input('Введите действие: '))
+
+            while command != 4:
+                if command == 1:
+                    student.read_score()
+                if command == 2:
+                    student.read_HW()
+                if command == 3:
+                    message = str(input('Введите запрос: '))
+                    student.request_admin(message)
+                
+        if status == ' Admin':
+            print('Вы вошли как администратор. Введите необходимое действие:\n'
+            '1 - Добавление учетной записи\n'
+            '2 - Удаление учетной записи\n'
+            '3 - Чтение запроса\n'
+            '4 - Чтение логов'
+            '5 - Выход')
+            command = int(input('Введите действие: '))
+
+            while command != 5:
+                if command == 1:
+                    admin.create()
+                if command == 2:
+                    admin.delete()
+                if command == 3:
+                    admin.read_request()
+                if command == 4:
+                    admin.read_log()
+                
+
+        if login == ' ' and password == ' ':
+            lg.Log('User has selected exit')
             flag = False
-
-
-                    
-            
+    
